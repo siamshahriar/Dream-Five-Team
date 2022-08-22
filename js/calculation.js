60d51441ccd7d1e5d6fed55f242587972d560166
siamshahriar <siamshahriar99@gmail.com>
2022-08-22 18:16:54 +0600

    functions are applied now for betterment

--- js/calculation.js
+++ js/calculation.js
@@ -1,33 +1,41 @@
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    const perPlayerString = document.getElementById('per-player');
-    const perPlayer = parseInt(perPlayerString.value);
+function getIntValueOfTextField(field) {
+  const perPlayerString = document.getElementById(field);
+  const perPlayer = parseInt(perPlayerString.innerText);
+  return perPlayer;
+}
 
+function getIntValueOfInputField(field) {
+  const manager = document.getElementById(field);
+  const managerInt = parseInt(manager.value);
+  return managerInt;
+}
 
-    const player = document.getElementById('selected-players');
-    const numberOfPlayers = player.children.length;
+function setInnerText(field, value) {
+  const playerExpenses = document.getElementById(field);
+  playerExpenses.innerText = value;
+}
 
-    const playerExpenses = document.getElementById('player-expenses');
-    const playerExpensesTotal = perPlayer * numberOfPlayers;
-    playerExpenses.innerText = playerExpensesTotal;
-    
-    
-})
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const perPlayer = getIntValueOfInputField("per-player");
 
+  const player = document.getElementById("selected-players");
+  const numberOfPlayers = player.children.length;
 
+  const playerExpensesTotal = perPlayer * numberOfPlayers;
 
-document.getElementById('calculate-total-btn').addEventListener('click', function(){
-    const playerExpenses = document.getElementById('player-expenses');
-    const playerExpensesInt = parseInt(playerExpenses.innerText);
-    
-    const manager = document.getElementById('manager-field');
-    const managerInt = parseInt(manager.value);
+  setInnerText("player-expenses", playerExpensesTotal);
+});
 
-    const coach = document.getElementById('coach-field');
-    const coachInt = parseInt(coach.value);
+document
+  .getElementById("calculate-total-btn")
+  .addEventListener("click", function () {
+    const playerExpensesInt = getIntValueOfTextField("player-expenses");
 
-    const lastTotal = document.getElementById('last-total-field');
-    lastTotal.innerText = playerExpensesInt + managerInt + coachInt;
+    const managerInt = getIntValueOfInputField("manager-field");
 
+    const coachInt = getIntValueOfInputField("coach-field");
 
+    const total = playerExpensesInt + managerInt + coachInt;
 
-})
+    setInnerText("last-total-field", total);
+  });
